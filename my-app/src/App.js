4ccb2fc05a3a9c870dca5669f612ab981e265362
tyu13 <tyu13@student.ubc.ca>
2024-03-09 14:29:20 -0800

    added router, put home in own page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,21 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import logo from './logo.svg';
+import Home from './pages/home/home.js'
 import './App.css';
 
 import HomeToBoardButton from './components/Button.js'; // Make sure to adjust the import path
 
 function App() {
-  const handleClick = () => {
-    console.log('CustomButton clicked!');
-    // You can perform any desired action here
-  };
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>
-          Welcome To The Community Board
-        </h1>
-        <HomeToBoardButton onClick={handleClick} label="Click Me To Find Community Boards Near You" color= "#efbbf0" />
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="blogs" element={<Blogs />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
