6fa159e57c8477fa8784f72a470be2076fccddf6
sooop <smoiseye@students.cs.ubc.ca>
2024-03-09 21:37:52 -0800

    addded map position selecting functionality

--- my-app/src/pages/map/map.js
+++ my-app/src/pages/map/map.js
@@ -1,27 +1,27 @@
-import React from 'react';
-import { GoogleMap, LoadScript } from '@react-google-maps/api';
+// import React from 'react';
+// import { GoogleMap, LoadScript } from '@react-google-maps/api';
 
 
-function Map() {
-    const center = {
-        lat: 49.26247856768246,
-        lng: -123.24547308038697
-      };
+// function Mapoo() {
+//     const center = {
+//         lat: 49.26247856768246,
+//         lng: -123.24547308038697
+//       };
 
-    return (
-        <LoadScript
-      googleMapsApiKey="AIzaSyByWfjubELtGoOn1nMQ6e26d95180yn3-w" // Replace with your API key
-    >
-      <GoogleMap
-        mapContainerStyle={{ width: '100vw' , height: '100vh' }}
-        center={center}
-        zoom={10}
-      >
-        {/* Child components, like markers, info windows, etc. */}
-      </GoogleMap>
-    </LoadScript>
-    );
+//     return (
+//         <LoadScript
+//       googleMapsApiKey="AIzaSyByWfjubELtGoOn1nMQ6e26d95180yn3-w" // Replace with your API key
+//     >
+//       <GoogleMap
+//         mapContainerStyle={{ width: '100vw' , height: '100vh' }}
+//         center={center}
+//         zoom={10}
+//       >
+//         {/* Child components, like markers, info windows, etc. */}
+//       </GoogleMap>
+//     </LoadScript>
+//     );
 
-}
+// }
 
-export default Map;
+// export default Mapoo;
