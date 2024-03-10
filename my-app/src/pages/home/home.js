165604b9c41e7c547c5cb07e80aad74d785a996a
sooop <smoiseye@students.cs.ubc.ca>
2024-03-09 16:28:56 -0800

    added cloud

--- my-app/src/pages/home/home.js
+++ my-app/src/pages/home/home.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import HomeToBoardButton from '../../components/Button';
 
+
 function Home() {
+    const handleClick = () => {
+        console.log('CustomButton clicked!');
+        // You can perform any desired action here
+      };
     return(
+
+        
         <header className="App-header">
 
             <p>
             Welcome To The Community Board
             </p>
-            {/* <HomeToBoardButton onClick={} label="Click Me To Find Community Boards Near You" /> */}
+            <HomeToBoardButton onClick={handleClick} label="Click Me To Find Community Boards Near You" color="#efbbf0" />
         </header>
     );
 }
