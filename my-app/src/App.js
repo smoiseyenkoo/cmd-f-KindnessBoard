a0df082916ab766ed0b005cb3932a5e16b96450d
tyu13 <tyu13@student.ubc.ca>
2024-03-09 17:47:44 -0800

    map

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -10,14 +10,15 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 function App() {
 
   return (
-
-    <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/board" element={<Board />} />
-    </Routes>
-  </BrowserRouter>
+    <Map/>
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route path="/" element={<Home />} />
+    //     <Route path="/board" element={<Board />} />
+    //   </Routes>
+    // </BrowserRouter>
   );
 }
 
 export default App;
+
--- my-app/src/pages/map/map.js
+++ my-app/src/pages/map/map.js
@@ -1,15 +1,25 @@
 import React from 'react';
-import Disappear from './disappear';;
+import { GoogleMap, LoadScript } from '@react-google-maps/api';
 
 
 function Map() {
+    const center = {
+        lat: 49.26247856768246,
+        lng: -123.24547308038697
+      };
+
     return (
-        <div>
-            <Disappear/>
-            <h1>uwu</h1>
-
-
-        </div>
+        <LoadScript
+      googleMapsApiKey="AIzaSyByWfjubELtGoOn1nMQ6e26d95180yn3-w" // Replace with your API key
+    >
+      <GoogleMap
+        mapContainerStyle={{ width: '100vw' , height: '100vh' }}
+        center={center}
+        zoom={10}
+      >
+        {/* Child components, like markers, info windows, etc. */}
+      </GoogleMap>
+    </LoadScript>
     );
 
 }
