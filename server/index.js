a6b71b3a43cb1d99a91b898666a5c73c5c7eaa1f
jzhong16 <jooleyaz@gmail.com>
2024-03-09 16:11:05 -0800

    Initialize database

--- server/index.js
+++ server/index.js
@@ -1,12 +1,14 @@
-const{config} = require('dotenv')
-
-const express = require("express");
+import { config } from 'dotenv';
+import express from "express";
+import { executeBulletinOperations } from './bulletin.js';
 
 const app = express();
 const port = 8000;
 
 config();
 console.log(process.env.DB_URI);
+
+await executeBulletinOperations();
 
 app.get("/api", (req, res) => {
     res.json({ users: ["userOne", "userTwo", "userThree"] })
