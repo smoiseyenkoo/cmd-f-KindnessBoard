257cd3b6c8987a36f4a179ea255df2e819897b5d
sooop <smoiseye@students.cs.ubc.ca>
2024-03-09 19:21:08 -0800

    Revert "made new map button & put in top left corner"
    This reverts commit 2044ddecfff823160fdf96c90de782520c87ed81.

--- my-app/src/pages/home/home.js
+++ my-app/src/pages/home/home.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import HomeToBoardButton from '../../components/Button';
-
 import { useNavigate } from 'react-router-dom';
 
 function Home() {
@@ -16,10 +15,9 @@ function Home() {
         <header className="App-header">
 
             <p>
-            Welcome To Your Community Board
+            Welcome To The Community Board
             </p>
             <HomeToBoardButton onClick={handleClick} label="Click Me To Find Community Boards Near You" color="#efbbf0" />
-
         </header>
     );
 }
