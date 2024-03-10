19fe45d77b3f2e48eb74ccdc8666e6d7e124bc87
hkgulka <hkgulka@students.cs.ubc.ca>
2024-03-09 19:23:22 -0800

    finished database interaction methods

--- server/bulletin.js
+++ server/bulletin.js
@@ -1,6 +1,10 @@
 import { MongoClient } from 'mongodb';
 
-export async function connectToCluster(uri) {
+const uri = process.env.DB_URI;
+const database = 'boards';
+const ttl = 30; // the number of seconds a post lasts on a board
+
+export async function connectToCluster() {
     let mongoClient;
  
     try {
@@ -16,30 +20,101 @@ export async function connectToCluster(uri) {
     }
  }
  
- // post = json file of all things
- export async function createPost(collection, post) {
-    await collection.insertOne(post);
- }
-
-export async function findAllPosts(collection) {
-    return collection.find().toArray();
-}
-
-export async function executeBulletinOperations(board, post) {
-    const uri = process.env.DB_URI;
+// returns true if a board with that name already exists or if there is an error
+export async function boardExists(title) {
     let mongoClient;
+    let boardExists = true;
  
     try {
-        mongoClient = await connectToCluster(uri);
-        const db = mongoClient.db('boards');
-        const collection = db.collection(board);
-        collection.createIndex( { "createdAt": 1 }, { expireAfterSeconds: 30 } )
+        mongoClient = await connectToCluster();
+        const db = mongoClient.db(database);
+        boardExists = db.getCollectionNames().includes(title);
 
-        await createPost(collection, post);
-
-        console.log(await findAllPosts(collection));
+        if (boardExists) {
+            console.log(`Collection ${title} already exists!`);
+        } else {
+            console.log(`Collection ${title} does not yet exist.`);
+        }
     } finally {
         await mongoClient.close();
     }
- }
+    return boardExists;
+}
+
+// returns the location of a given board, an object with lon and lat
+// REQUIRES: the given board already exists!
+export async function getBoardLocation(title) {
+    let mongoClient;
+    let boardLocation = {};
+ 
+    try {
+        mongoClient = await connectToCluster();
+        const db = mongoClient.db(database);
+        const collection = db.collection(title);
+        boardLocation = await collection.findOne({ _id : "location"});
+
+    } finally {
+        await mongoClient.close();
+    }
+    return boardLocation;
+}
+
+// returns an array of all posts on the given board, or an empty array if none
+export async function getAllBoardPosts(title) {
+    let mongoClient;
+    let posts = [];
+ 
+    try {
+        mongoClient = await connectToCluster();
+        const db = mongoClient.db(database);
+        const collection = db.collection(title);
+
+        posts = await collection.find().toArray();
+
+        console.log(`Retrieving all posts on the ${title} board: ${posts}`);
+    } finally {
+        await mongoClient.close();
+    }
+    return posts;
+}
+
+// creates a board given a json object holding title, lat, and lon
+// returns true on success
+export async function createBoard(board) {
+    let mongoClient;
+    const title = board.title;
+    const location = { _id: "location", lat: board.lat, lon: board.lon };
+ 
+    try {
+        mongoClient = await connectToCluster();
+        const db = mongoClient.db(database);
+        const collection = db.collection(title);
+        await collection.createIndex( { "createdAt": 1 }, { expireAfterSeconds: ttl } );
+        await collection.insertOne(location);
+
+        console.log(`Created new board: ${title}`);
+    } finally {
+        await mongoClient.close();
+    }
+    return true;
+}
+
+// creates a post on a given board, where post is a json object
+// returns true on success
+export async function createPost(title, post) {
+    let mongoClient;
+ 
+    try {
+        mongoClient = await connectToCluster();
+        const db = mongoClient.db(database);
+        const collection = db.collection(title);
+        
+        await collection.insertOne(post);
+
+        console.log(`New post added to the ${title} board: ${post.title}`);
+    } finally {
+        await mongoClient.close();
+    }
+    return true;
+}
 
--- server/index.js
+++ server/index.js
@@ -8,16 +8,20 @@ const port = 8000;
 config();
 console.log(process.env.DB_URI);
 
-const test = {"createdAt": new Date(),
-    name : "hannah",
-text : "hello i am hannah",
-number : 1}
-
-await executeBulletinOperations("hi", test);
-
+// get: returns true if the board exists, false
 app.get("/api", (req, res) => {
     res.json({ users: ["userOne", "userTwo", "userThree"] })
 });
+
+// get: returns the location of an existing board
+
+// get: returns all posts on an existing board
+
+// post: creates a board, given a board with that title doesn't already exist
+
+// post: creates a post on an existing board
+
+
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
