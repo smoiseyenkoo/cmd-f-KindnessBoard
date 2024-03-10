343632578923802fa68a073d03a48e35267289d2
sooop <smoiseye@students.cs.ubc.ca>
2024-03-10 11:33:11 -0700

    added whitespace before board title for aesthetics

--- my-app/src/pages/board/board.js
+++ my-app/src/pages/board/board.js
@@ -43,11 +43,11 @@ function Board() {
 
     const addSticky = () => {
         const maxX = 800;
-        const maxY = 600;
+        const maxY = 500;
         const newSticky = {
             id: stickies.length,
             x: Math.random() * maxX + 190,
-            y: Math.random() * maxY + 100,
+            y: Math.random() * maxY + 150,
             text: "..."
         };
 
@@ -59,6 +59,10 @@ function Board() {
 
     const handleClick = () => {
         navigate('/map');
+    };
+
+    const handleHome = () => {
+        navigate('/');
     };
 
     const boardStyle = {
@@ -91,9 +95,11 @@ function Board() {
             <div style={{ position: 'absolute', top: 0, right: 0, margin: '10px' }}>
                 <GenericButton onClick={addSticky} label="click to add a new sticky" color="#fac8f2" />
             </div>
+            <div style={{ position: 'absolute', top: 50, right: 0, margin: '10px' }}>
+                <GenericButton onClick={handleHome} label="home" color="#fac8f2" />
+            </div>
 
             {stickies.map((sticky) => (
-                // <StickyButton onClick={goToStickyNote} key={sticky.id} 
                 <StickyButton onClick={() => handleOpen(sticky.id)}
                     style={{ left: sticky.x + 'px', top: sticky.y + 'px' }}
                 />
@@ -105,12 +111,10 @@ function Board() {
                 aria-labelledby="modal-modal-title"
                 aria-describedby="modal-modal-description"
             >
-                {/* <Box sx={style}> */}
                 <div>
                     <BigSticky handleClose={handleClose} setMessage={setMessage} message={message} />
 
                 </div>
-                {/* </Box> */}
             </Modal>
 
 
